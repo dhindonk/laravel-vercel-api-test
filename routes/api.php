99359bf85cf889a289c79b85cc29dab2c10988eb
dhindonk <mfahdin1803@gmail.com>
2024-06-07 10:13:43 +0700

    fix

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\SensorDataController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+ 
 
-
-Route::get('/tes', function(Request $request){
-    return response()->json([
-        'message' => 'sukses pak'
-    ]);
-});
+Route::post('/sensor-data', [SensorDataController::class, 'store']);
