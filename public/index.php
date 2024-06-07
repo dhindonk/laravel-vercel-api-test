b692da55c0d45cbad6009895a3971d1e19ba60bc
dhindonk <mfahdin1803@gmail.com>
2024-06-07 12:41:43 +0700

    fix

--- public/index.php
+++ public/index.php
@@ -16,6 +16,7 @@ require __DIR__ . '/../vendor/autoload.php';
 (require_once __DIR__ . '/../bootstrap/app.php')
     ->handleRequest(Request::capture());
 
+
 // Create temporary directories if they don't exist
 $directories = [
     '/tmp/views',
